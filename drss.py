fd5f3628ac903b55b2507cec048a289f2c0ca3e7
nulvox <38874956+nulvox@users.noreply.github.com>
2025-02-17 18:30:29 -0500

    Update deluge web client calls
    use deluge_web_client instead of requests
    build out actual request logic
    run black formatter

--- drss.py
+++ drss.py
@@ -1,24 +1,46 @@
 #!/usr/bin/env python3
 import argparse, feedparser, json, requests, urllib
+from deluge_web_client import DelugeWebClient
 from os import environ
 
-# 1. set up argparse (and parse input)
-#   a list file for feeds
-#   b json file for limits
-#   c client secrets file (or env vars? :shrug:)
-#   d think about filter rules file?
 parser = argparse.ArgumentParser()
-parser.add_argument("-F", "--feeds-file", help="Path to a file containing a list of RSS feed URLs 1 URL per line", required=True)
-parser.add_argument("-L", "--limits-file", help="Path to a json file describing the limits, sane defaults will be selected.", required=False)
-#parser.add_argument("-R", "--rules-file", help="Path to a file with the rss content filter rules... tbd")
-parser.add_argument("-u", "--username", help="Deluge webui username, deffaults to env $DUN and falls back to 'admin'", default=environ.get('DUN', default='admin'))
-parser.add_argument("-p", "--password", help="Deluge webui password, defaults to env $DPW and will not proceed without a value supplied.", default=environ.get('DPW', default=''))
-parser.add_argument("-d", "--deluge-url", help="URL to the deluge instance, defaults to env $DURL and falls back to '127.0.0.1:8112'", default=environ.get('DURL', default='http://127.0.0.1:8112'))
+parser.add_argument(
+    "-F",
+    "--feeds-file",
+    help="Path to a file containing a list of RSS feed URLs 1 URL per line",
+    required=True,
+)
+parser.add_argument(
+    "-L",
+    "--limits-file",
+    help="Path to a json file describing the limits, sane defaults will be selected.",
+    required=False,
+)
+# parser.add_argument("-R", "--rules-file", help="Path to a file with the rss content filter rules... tbd")
+parser.add_argument(
+    "-u",
+    "--username",
+    help="Deluge webui username, deffaults to env $DUN and falls back to 'admin'",
+    default=environ.get("DUN", default="admin"),
+)
+parser.add_argument(
+    "-p",
+    "--password",
+    help="Deluge webui password, defaults to env $DPW and will not proceed without a value supplied.",
+    default=environ.get("DPW", default=""),
+)
+parser.add_argument(
+    "-d",
+    "--deluge-url",
+    help="URL to the deluge instance, defaults to env $DURL and falls back to '127.0.0.1:8112'",
+    default=environ.get("DURL", default="http://127.0.0.1:8112"),
+)
 args = parser.parser_args()
 
 # ensure creds were supplied to do the work
 if args.password is None:
-    raise ValueError('$DPW and -p are not defined with a valid deluge Webui password')
+    raise ValueError("$DPW and -p are not defined with a valid deluge Webui password")
+
 
 # set up a file size string interpreter
 class fsi:
@@ -29,64 +51,61 @@ class fsi:
         size_str = size_str.strip().upper()
         for unit in cls.units:
             if size_str.endswith(unit):
-                number = float(size_str[:-len(unit)].strip())
+                number = float(size_str[: -len(unit)].strip())
                 return int(number * cls.units[unit])
         raise ValueError("Invalid file size string")
 
-# confirm the deluge service is running and accepts the supplied creds
-def test_deluge_service(un, pw, url):
-    pass
-
-# get the current deluge context
-def get_deluge_context(un, pw, url):
-    pass
 
 # supply the existing deluge context and a given torrentspec
 # return a bool for if it is already active
 def already_active(torrent, context):
-    pass
+    target_name = torrent["title"]
+    for tid, info in context:
+        if info["name"] == target_name:
+            return True
+    return False
 
-# 2. set up the Deluge API client
-#   2.5 build the API request template dict to set seeding rules
-def new_deluge_client():
-    pass
 
-# 3. open the feed list file and generate the list object
+# Check how much the disk quota will overflow if we add the torrent
+def check_limit(delta):
+    free_space = dc.get_free_space(path="rss")
+    return float(delta) - float(fsi.interpret(free_space))
+
+
+# open the feed list file and generate the list object
 def get_feed_list(feed_file):
-    with open(feed_file, 'r') as feeds:
+    with open(feed_file, "r") as feeds:
         return feeds.readlines()
 
-# 4. begin for loop on feed list
-# 5. set up the rssfeed parser block with supplied feed arg
-# 6. (bonus) add secondary filtering schema?
-# 7. evaluate active torrent and disk limits
-# 8. call the api to add relevant torrent
-# 9. log the event
-# 10. close the loop, and exit
 
 def main():
     # ensure the deluge service is running and likes your creds
-    test_deluge_service()
-    feedlist = get_feed_list(args.feeds-file)
+    dc = DelugeWebClient(url=args.delugeurl, password=args.password)
+    dc.login()
+    context = dc.get_torrents_status(keys=["name"])
+    feedlist = get_feed_list(args.feeds)
     if not feedlist:
-        raise ValueError('No feed list supplied.')
-    limitsdict = get_limits_dict(args.limits-file)
-    limits_keys = list(limitsdict.keys())
-    missing_fileds = []
-    for key in limits_req_fields:
-        if field not in limits_keys:
-            missing_fileds.append(key)
-    if missing_fields:
-        raise ValueError(f'The following required fields were not defined in the supplied limits file: {missing_fileds}')
+        raise ValueError("No feed list supplied.")
+    limitsdict = get_limits_dict(args.limits)
+    # limits_keys = list(limitsdict.keys())
+    # missing_fileds = []
+    # @TODO set up required fields
+    # for key in limits_req_fields:
+    #    if field not in limits_keys:
+    #        missing_fileds.append(key)
+    # if missing_fields:
+    #    raise ValueError(f'The following required fields were not defined in the supplied limits file: {missing_fileds}')
     # loop over RSS feeds to work our magic
     for feed in feedlist:
         # runs check_limit on self
         if not check_limit(0):
             # breaks the loop if the limit is already met or exceeded
-            print("Deluge Server limits already met or exceeded, ending feed processing.")
+            print(
+                "Deluge Server limits already met or exceeded, ending feed processing."
+            )
             break
         # build the feed parser
-        try: 
+        try:
             urllib.parse.urlparse(feed)
         except:
             print(f"Invalid url {feed} in list, continuing loop...")
@@ -95,21 +114,35 @@ def main():
             content = feedparser.parser(feed)
         except:
             print(f"Cloud not fetch rss feed {feed}, continuing loop..")
-        for torrent in content['entries']:
-			if check_limit(0) > 0:
-				print("Deluge server limits already met or exceeded, ending feed processing for {feed}.")
-				break
-			if already_active(torrent, context):
+        for torrent in content["entries"]:
+            if check_limit(0) > 0:
+                print(
+                    "Deluge server limits already met or exceeded, ending feed processing for {feed}."
+                )
+                break
+            if already_active(torrent, context):
                 print(f"Torrent {torrent} is already active, continuing loop...")
                 continue
-            sizeh = torrent['summary_detail']['value'].split(';')[0]
-			size = fsi.interpret(sizeh)
-			if check_limit(size) > 0:
-				print(f"Torrent {torrent} with size {sizeh} exceeds the remaining limits, continuing loop...")
-				continue
-			# add secondary filters here (ie, stop hosting TSwizzle)
-			add_torrent(torrent, args.username, args.password, args.durl)
-			print(f"adding torrent {torrent}")
+            sizeh = torrent["summary_detail"]["value"].split(";")[0]
+            size = fsi.interpret(sizeh)
+            if check_limit(size) > 0:
+                print(
+                    f"Torrent {torrent} with size {sizeh} exceeds the remaining limits, continuing loop..."
+                )
+                continue
+            # add secondary filters here (ie, stop hosting TSwizzle)
+            torrent_file = download_torrentfile(torrent)
+            upload = dc.upload_torrent(
+                torrent_path=torrent_file,
+                add_paused=False,
+                label="drss-managed",
+                seed_mode=False,
+                auto_managed=True,
+            )
+            if not upload["error"]:
+                print(f"adding torrent {torrent} with hash {upload['result']}")
+                context = dc.get_torrents_status(keys=["name"])
+
 
 if __name__ == "__main__":
     main()
